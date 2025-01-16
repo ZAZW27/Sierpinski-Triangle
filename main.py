2f726eed88073398f66acb94a9c21928d8a4f5f6
Zhorif <zharifazizzulkarnain@gmail.com>
2025-01-16 22:42:36 +0800

    added functions

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import matplotlib.pyplot as plt
+import random
 
 points = [(-50, -30), (0, 70), (50, -30), (-50, -30)]
-
+initDot = (30, -19)
 x, y = zip(*points)
 
-plt.plot(x, y, color='black', label='Sierpinski triangle')
+def createTriangle(): 
+    plt.plot(x, y, color='black', label='Sierpinski triangle')
+    
+    calculateDots()
+    
+def calculateDots(): 
+    plt.plot(initDot[0], initDot[1], marker='o', markersize=2 , color='black')
+    
+    for _ in range(0, 100):
+        pickCorner = random.randint(0, 2)
+        prevDot = [initDot[0], initDot[1]]
+        xDot = float((prevDot[0] + x[pickCorner]) / 2)
+        yDot = float((prevDot[1] + y[pickCorner]) / 2)
+        
+        plt.plot(xDot, yDot, marker='o', markersize=2 , color='black')
+    
 
-for i in range(0, 10): 
-    plt.plot(i+10, i+10, marker='o', markersize=2 , color='black')
+def main(): 
+    createTriangle()
+    
+    plt.xlabel('x label')
+    plt.ylabel('y label')
+    plt.title("Coba")
 
-plt.xlabel('x label')
-plt.ylabel('y label')
-plt.title("Coba")
+    plt.legend()
 
-plt.legend()
+    plt.xticks(range(-50, 60, 1))
+    plt.yticks(range(-30, 80, 1))
 
-plt.xticks(range(-50, 60, 1))
-plt.yticks(range(-30, 80, 1))
-
-plt.grid(False)
-plt.show()
+    plt.grid(False)
+    plt.show()
+    
+main()
